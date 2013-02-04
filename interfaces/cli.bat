ea651a192c420a618167bacf6c38238405a87dbd
tcanvn <tcanvn@gmail.com>
2013-02-04 03:01:47 +0000

    Upadte "cli.bat" and "cli.sh" so that they will check for runnable jar file existence before calling it. This file can be in the same folder or in the "target" folder.

--- interfaces/cli.bat
+++ interfaces/cli.bat
@@ -1,7 +1,22 @@
 @echo off
 
-echo ------------------------------------
-echo --- Configuration file: %1
-echo ------------------------------------
+IF EXIST "dl-learner.jar" (
+	
+	echo ------------------------------------
+	echo --- Configuration file: %1
+	echo ------------------------------------
 
-java -Xmx2G -jar target\dl-learner.jar %1
+	java -Xmx1G -jar dl-learner.jar %1
+) ELSE (
+	IF EXIST "target\dl-learner.jar" (
+		echo ------------------------------------
+		echo --- Configuration file: %1
+		echo ------------------------------------
+		
+		java -Xmx1G -jar target\dl-learner.jar %1
+		
+	) ELSE (
+	 	echo "dl-learner.jar" is not found.
+	 	echo Recompile this project and try again.
+	)
+)
