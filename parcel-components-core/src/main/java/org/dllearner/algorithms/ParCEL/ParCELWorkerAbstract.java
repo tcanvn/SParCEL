6a7de833e8461e7f016498fa35649720bbdc5111
tcanvn <tcanvn@gmail.com>
2013-02-21 04:57:23 +0000

    Eclipse version of ParCEL and DLLearner. Launch configuration files are provided.

--- parcel-components-core/src/main/java/org/dllearner/algorithms/ParCEL/ParCELWorkerAbstract.java
+++
@@ -1,8 +0,0 @@
-package org.dllearner.algorithms.ParCEL;
-
-public abstract class ParCELWorkerAbstract implements Runnable {
-	
-	// name of worker (for debugging purpose)
-	protected String name;
-
-}
