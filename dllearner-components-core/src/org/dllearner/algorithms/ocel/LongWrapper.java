47a2097362105e62367d9081d536bb10416d1fc6
tcanvn <tcanvn@gmail.com>
2013-02-21 06:12:49 +0000

    commit dllearner-components-core project (missing from the previous commit)

---
+++ dllearner-components-core/src/org/dllearner/algorithms/ocel/LongWrapper.java
@@ -0,0 +1,22 @@
+package org.dllearner.algorithms.ocel;
+
+public class LongWrapper {
+	long value;
+	
+	public LongWrapper() {
+		
+	}
+	
+	public LongWrapper(long v) {
+		this.value = v;
+	}
+	
+	public long getValue() {
+		return this.value;
+	}
+	
+	public void setValue(long v) {
+		this.value = v;
+	}
+	
+}
