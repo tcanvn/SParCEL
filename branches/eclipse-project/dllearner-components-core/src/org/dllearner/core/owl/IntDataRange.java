e3d48488ca0426b7434a5802729711b073353e77
tcanvn@gmail.com <tcanvn@gmail.com@eeb7d0ac-7374-14cc-3b4b-7afbc90e1cdf>
2013-02-21 06:12:49 +0000

    commit dllearner-components-core project (missing from the previous commit)

---
+++ branches/eclipse-project/dllearner-components-core/src/org/dllearner/core/owl/IntDataRange.java
@@ -0,0 +1,13 @@
+package org.dllearner.core.owl;
+
+/**
+ * Integer data range, used in integer datatype restriction
+ * 
+ * @author An C. Tran
+ *
+ */
+public interface IntDataRange extends DataRange {
+
+	public int getValue();
+	
+}
